6c05145038f0cbe76a87526be00f0c8c58d26880
Dominic Bennett <dominic.john.bennett@gmail.com>
2018-12-11 14:21:39 +0100

    using script to execute commands

--- R/functions.R
+++ R/functions.R
@@ -8,14 +8,20 @@
 mafft <- function(...) {
   arglist <- outsider::.arglist_get(...)
   files_to_send <- outsider::.filestosend_get(arglist)
+  arglist <- outsider::.arglist_parse(arglist)
+  # write arglist as script
+  script_flpth <- file.path(tempdir(), 'script')
+  write(x = paste(c('mafft', arglist), collapse = ' '), file = script_flpth)
+  on.exit(file.remove(script_flpth))
   otsdr <- outsider::.outsider_init(repo = 'dombennett/om..mafft',
-                                    cmd = 'mafft', wd = getwd(),
-                                    files_to_send = files_to_send,
-                                    arglist = arglist)
-  if (arglist == '--help') {
+                                    cmd = 'bash', wd = getwd(),
+                                    files_to_send = c(files_to_send,
+                                                      script_flpth),
+                                    arglist = 'script')
+  if ('--help' %in% arglist) {
     # mafft raises an error for --help
-    try(outsider::.run(otsdr), silent = TRUE)
-    res <- TRUE
+    otsdr$ignore_errors <- TRUE
+    outsider::.run(otsdr)
   } else {
     res <- outsider::.run(otsdr)
   }
